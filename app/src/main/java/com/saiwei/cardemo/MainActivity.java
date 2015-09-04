b750c606e85da3c549472799568e17335bca14ce
saiwei <978447713@qq.com>
2015-09-04 13:37:37 +0800

    version : 1.2.6     在地图上添加标记

--- app/src/main/java/com/saiwei/cardemo/MainActivity.java
+++ app/src/main/java/com/saiwei/cardemo/MainActivity.java
@@ -15,6 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -25,10 +26,16 @@ import com.baidu.location.LocationClientOption;
 import com.baidu.location.Poi;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BaiduMapOptions;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.InfoWindow;
 import com.baidu.mapapi.map.MapStatus;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.Marker;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.OverlayOptions;
 import com.baidu.mapapi.map.SupportMapFragment;
 import com.baidu.mapapi.model.LatLng;
 import com.ikimuhendis.ldrawer.ActionBarDrawerToggle;
@@ -50,6 +57,12 @@ public class MainActivity extends FragmentActivity {
     public LocationClient mLocationClient;
     public MyLocationListener mMyLocationListener;
 
+    private Marker mMarkerA;
+
+    // 初始化全局 bitmap 信息，不用时及时 recycle
+    BitmapDescriptor bdA = BitmapDescriptorFactory
+            .fromResource(R.drawable.icon_marka);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,6 +77,7 @@ public class MainActivity extends FragmentActivity {
         initMap();
         initList();
         initLocation();
+        initMarker();
     }
 
     /**
@@ -144,25 +158,48 @@ public class MainActivity extends FragmentActivity {
         mLocationClient.registerLocationListener(mMyLocationListener);
 
         LocationClientOption option = new LocationClientOption();
-//        option.setLocationMode(tempMode);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
-//        option.setCoorType(tempcoor);//可选，默认gcj02，设置返回的定位结果坐标系，
-//        int span=1000;
-//        try {
-//            span = Integer.valueOf(frequence.getText().toString());
-//        } catch (Exception e) {
-//            // TODO: handle exception
-//        }
-//        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
-//        option.setIsNeedAddress(checkGeoLocation.isChecked());//可选，设置是否需要地址信息，默认不需要
-//        option.setOpenGps(true);//可选，默认false,设置是否使用gps
-//        option.setLocationNotify(true);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
-//        option.setIgnoreKillProcess(true);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
-//        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤gps仿真结果，默认需要
-//        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
-//        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
         mLocationClient.setLocOption(option);
 
         startLoc();
+    }
+
+    LatLng mMapCenter = null;
+
+    private void initMarker(){
+        mMapCenter = new LatLng(26.169348, 118.194487);
+
+        OverlayOptions ooA = new MarkerOptions().position(mMapCenter).icon(bdA)
+                .zIndex(9);
+        mMarkerA = (Marker) (mBaiduMap.addOverlay(ooA));
+
+        mBaiduMap.setOnMarkerClickListener(new BaiduMap.OnMarkerClickListener() {
+            public boolean onMarkerClick(final Marker marker) {
+
+                if(marker == mMarkerA){
+                    Toast.makeText(getApplicationContext(),"A　被点击",Toast.LENGTH_SHORT).show();
+                }
+
+//                Button button = new Button(getApplicationContext());
+//                button.setBackgroundResource(R.drawable.popup);
+//                InfoWindow.OnInfoWindowClickListener listener = null;
+//                if (marker == mMarkerA) {
+//                    button.setText("更改位置");
+//                    listener = new InfoWindow.OnInfoWindowClickListener() {
+//                        public void onInfoWindowClick() {
+//                            LatLng ll = marker.getPosition();
+//                            LatLng llNew = new LatLng(ll.latitude + 0.005,
+//                                    ll.longitude + 0.005);
+//                            marker.setPosition(llNew);
+//                            mBaiduMap.hideInfoWindow();
+//                        }
+//                    };
+//                    LatLng ll = marker.getPosition();
+//                    mInfoWindow = new InfoWindow(BitmapDescriptorFactory.fromView(button), ll, -47, listener);
+//                    mBaiduMap.showInfoWindow(mInfoWindow);
+//                }
+                return true;
+            }
+        });
     }
 
     /**
